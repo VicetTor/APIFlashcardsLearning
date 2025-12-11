8a758bfb411ae9ceb5e12d95e2a85fd9182bfe87
Victor <victoryvray@gmail.com>
2025-12-11 08:38:57 +0100

    Starting of a structure

---
+++ src/db/schema.js
@@ -0,0 +1,26 @@
+import { sqliteTable, text, integer, boolean } from "drizzle-orm/sqlite-core"
+import { randomUUID } from 'crypto'
+
+export const user = sqliteTable('user',{
+    id: text().primaryKey().$defaultFn(()=>randomUUID()),
+    mail: text('email', {length:255}).notNull().unique(),
+    firstName: text('first_name',{length:50}).notNull(),
+    lastName: text('last_name',{length:50}).notNull(),
+    password: text('password', {length:255}).notNull(),
+    isAdmin: boolean('is_admin').notNull(),
+    createdAt: integer('created_at', { mode: 'timestamp'}).$defaultFn(
+        () => new Date()
+    ),
+})
+
+export const collection = sqliteTable('collection'{
+    id: text().primaryKey().$defaultFn(()=>randomUUID()),
+    title: text('title', {length:100}).notNull(),
+    description: text('description', {length:255}),
+    isPublic: boolean('is_public').notNull(),
+    createdAt: integer('created_at', { mode: 'timestamp'}).$defaultFn(
+        () => new Date()
+    ),
+    owner: text('owner').references(()=> user.id,{ onDelete: 'cascade' }).notNull()
+})
+
