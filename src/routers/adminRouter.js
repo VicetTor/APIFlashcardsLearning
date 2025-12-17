34aefb5ebeb66a1207b295c222a03acd1c13c527
BEUVE <beuve225@unicaen.fr>
2025-12-17 14:25:43 +0100

    add authenticateToken

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express"
 import  {getAllUsers} from "../controllers/adminController.js"
+import { authenticateToken } from '../middlewares/authMiddleware.js'
 
 const router = Router()
+router.use(authenticateToken)
 
 router.get('/users', getAllUsers)
 
