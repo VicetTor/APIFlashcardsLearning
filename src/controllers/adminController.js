f00d716edccf43a1b4e0632359844c5a9392f234
BEUVE <beuve225@unicaen.fr>
2026-01-04 11:24:57 +0100

    list of users in order of creation date

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,6 +1,6 @@
 import { db } from '../db/database.js'
 import { users } from '../db/schema.js'
-import { eq } from "drizzle-orm"
+import { eq, desc } from "drizzle-orm"
 import 'dotenv/config'
 
 export const getAllUsers = async(req, res) => {
@@ -8,8 +8,8 @@ export const getAllUsers = async(req, res) => {
         const idCurentUser = req.user.userId
         const [user] = await db.select().from(users).where(eq(users.id,idCurentUser))
         console.log("users: " + user.isAdmin)
-        if(user.isAdmin == 1){
-            const [listUsers] = await db.select().from(users)
+        if(user.isAdmin){
+            const listUsers = await db.select().from(users).orderBy(desc(users.createdAt))
         
         if(!listUsers){
             return res.status(401).json({error: 'no users'})
