9a32f6b9cff276325bc8561f92ace28688c4c5b8
Victor YVRAY <victoryvray@gmail.com>
2026-01-04 11:36:12 +0100

    Deleting user by admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -26,3 +26,29 @@ export const getAllUsers = async(req, res) => {
         })
     }
 }
+
+/**
+ * @param {request} req
+ * @param {response} res
+ */
+export const deleteUser = async(req, res) => {
+    const idCurrentUser = req.user.userId
+    const { idUserToDelete } = req.params
+   
+    try{
+        const [user] = await db.select().from(users).where(eq(users.id,idCurrentUser))
+        if(!user.isAdmin || (idUserToDelete == idCurrentUser)){
+            res.status(403).send({error:"Vous n'avez pas le droit de réaliser cette action"})
+        }
+        else{
+            const results = await db.delete(users).where(eq(users.id,idUserToDelete)).returning({id:users.id})
+            res.status(200).send({message: 'User deleted', data:results})
+        }
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).send({
+            error: 'deleteUser failed',
+        })
+    }
+}
