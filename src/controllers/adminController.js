3bfc0db8ca245a9b8f03ff6a255e9a8953f03b59
BEUVE <beuve225@unicaen.fr>
2026-01-04 11:00:21 +0100

    list of users as admin

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,16 +1,22 @@
 import { db } from '../db/database.js'
 import { users } from '../db/schema.js'
+import { eq } from "drizzle-orm"
+import 'dotenv/config'
 
 export const getAllUsers = async(req, res) => {
     try {
-        const curentUser = req.user.userId
-        console.log(curentUser)
-        const listUsers = await db.select().from(users)
+        const idCurentUser = req.user.userId
+        const [user] = await db.select().from(users).where(eq(users.id,idCurentUser))
+        console.log("users: " + user.isAdmin)
+        if(user.isAdmin == 1){
+            const [listUsers] = await db.select().from(users)
         
         if(!listUsers){
             return res.status(401).json({error: 'no users'})
         }
         res.status(200).json(listUsers)
+        }
+        
 
     }
     catch(error){
